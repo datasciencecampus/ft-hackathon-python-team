5042c60d3146d7701c355157817745555a5c3b64
Nathan Williams <nathan.williams@ons.gov.uk>
2023-06-12 17:13:22 +0100

    add options sidepane

--- src/hackathon/sample_ui.py
+++ src/hackathon/sample_ui.py
@@ -4,6 +4,7 @@
 import customtkinter as ctk
 from src.hackathon.utils.words import word_def_pair, get_definition
 from src.hackathon.utils.quit import quit_game
+from src.hackathon.utils.in_work_mode import boss_is_watching
 # %% Functions
 
 # TODO: tidy this up
@@ -64,11 +65,68 @@ ctk.set_default_color_theme("blue")
 # Begin with blank window
 root = ctk.CTk()
 
+# What to do when the X is clicked
+root.protocol('WM_DELETE_WINDOW', lambda: quit_game(root))
+
 # Window name
 root.title("Team FinTrans Wordle")
 
-# What to do when the X is clicked
-root.protocol('WM_DELETE_WINDOW', lambda: quit_game(root))
+
+root.grid_columnconfigure(1, weight=1)
+root.grid_rowconfigure((0, 1, 2), weight=1)
+
+# =============================================================================
+# PANE 1
+# =============================================================================
+# This frame holds config options
+sidebar = ctk.CTkFrame(root, width=50)
+
+# It needs a weight in order to do stuff in
+sidebar.grid_rowconfigure(0, weight=1)
+sidebar.grid_columnconfigure(0, weight=1)
+
+sidebar.grid(row=0,
+             column=0,
+             rowspan=10,
+             sticky='nsew')
+
+# Initial value
+boss_switch = ctk.StringVar(value='no')
+boss_watch = ctk.CTkSwitch(sidebar,
+                           text="Boss is in?",
+                           command=lambda: boss_is_watching(boss_switch),
+                           variable=boss_switch,
+                           onvalue="yes",
+                           offvalue="no",
+                           border_color=(WHITE, BLACK),
+                           bg_color='transparent')
+
+boss_watch.grid(row=NUM_GUESSES+2, column=0, padx=20, pady=0)
+# Text above option menu
+ui_scale = ctk.CTkLabel(sidebar, text='Scaling:', anchor='w')
+ui_scale.grid(row=NUM_GUESSES-1, column=0, padx=20, pady=10)
+# Clickable options
+ui_scale_options = ctk.CTkOptionMenu(sidebar,
+                                     values=[f'{i}%' for i in range(80, 130, 10)],
+                                     command = change_scaling)
+ui_scale_options.grid(row=NUM_GUESSES, column=0, padx=20, pady=0)
+ui_scale_options.set('100%')
+
+
+switch_var = ctk.StringVar(value="on")
+theme = ctk.CTkSwitch(sidebar,
+                      text="Dark mode",
+                      command=lambda: change_appearance(switch_var),
+                      variable=switch_var,
+                      onvalue="Dark",
+                      offvalue="Light",
+                      border_color=(WHITE, BLACK),
+                      bg_color='transparent')
+theme.select()
+
+theme.grid(row=NUM_GUESSES+1, column=0, padx=20, pady=(10, 10), sticky='s')
+
+
 # Generate 5x6 grid
 for row in range(NUM_GUESSES):
     for column in range(WORD_LENGTH):
