cc953269268d2e94c326268503625d8b007b9cdb
Nathan Williams <nathan.williams@ons.gov.uk>
2023-06-21 16:36:34 +0100

    WIP: fix for dup letters

--- src/hackathon/utils/logic.py
+++ src/hackathon/utils/logic.py
@@ -29,24 +29,31 @@ def n_letter(word, letter):
     n = word.count(letter)
     return n
 
-def check_placement_letter(word, idx, target):
-    print(word[idx])
-    if n_letter(word, word[idx]) == 1 or n_letter(target,word[idx]) == n_letter(word,word[idx]) or word[idx] not in word[0:idx]:
-        if word[idx] == target[idx]:
-            print(f'{word[idx]} is in the correct position')
+def check_placement_letter(word, target):
+
+    status = []
+    for idx, letter in enumerate(word):
+        if letter == target[idx]:
+            print(f'{letter} is in the correct position')
             color = GREEN
-        elif word[idx] in target and word[idx] != target[idx]:
-            print(f'{word[idx]} is in the word but in the wrong position')
-            color = YELLOW
-        elif word[idx] not in target:
-            print(f'{word[idx]} not in word')
-            color = GREY
-    elif n_letter(word, word[idx]) > 1 and n_letter(target, word[idx]) <= 1:
-        if word[idx] == target[idx]:
-            print(f'{word[idx]} is in the correct position')
-            color = GREEN
+        elif letter in target:
+            if word.count(letter) > 1 and word.count(letter) == target.count(letter):
+                print(f'{letter} is in the word but in the wrong position')
+                color = YELLOW
+            else:
+                print(f'{letter} not in word')
+                color = GREY
         else:
-            print(f'{word[idx]} not in word')
+            print(f'{letter} not in word')
             color = GREY
-    
-    return color
+
+        status.append((idx, letter, color))
+
+    return status
+
+if __name__ == '__main__':
+    word = 'HELLO'
+    target = 'WORLD'
+
+    output = check_placement_letter(word, target)
+
