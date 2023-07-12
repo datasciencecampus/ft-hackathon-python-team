98b06215881f9eaccc09732de66e2f082adfd5ed
Nathan Williams <nathan.williams@ons.gov.uk>
2023-07-12 20:25:39 +0100

    now equivalent to functional approach

--- src/hackathon/oop_attempt.py
+++ src/hackathon/oop_attempt.py
@@ -7,12 +7,13 @@ Created on Tue Jul 11 14:10:12 2023
 
 # %% Functions
 import customtkinter as ctk
-from typing import Union, Tuple, Callable, Optional, Dict
+from CTkMessagebox import CTkMessagebox
+
+# from typing import Union, Tuple, Callable, Optional, Dict
 
 from src.hackathon.utils.words import word_def_pair, get_definition
 from src.hackathon.utils.in_work_mode import boss_is_watching
 from src.hackathon.utils.appearance import change_appearance
-from src.hackathon.utils.logic import get_colours
 import src.hackathon.utils.constants as c
 
 from itertools import product
@@ -22,6 +23,7 @@ from itertools import product
 ctk.set_appearance_mode('dark')
 # Default theme
 ctk.set_default_color_theme("green")   
+      
 
 # Main Frame
 class Main(ctk.CTkFrame):
@@ -35,7 +37,7 @@ class Main(ctk.CTkFrame):
                        )
 
         self.grid(row = 0,
-                  rowspan = max(c.SPAN),
+                  rowspan = c.NUM_GUESSES,
                   column = 1)
         
         self.grid_coords = {}
@@ -60,6 +62,7 @@ class Main(ctk.CTkFrame):
         for row, col in product(range(parent.number_guesses), 
                                 range(parent.word_length)
                                 ):
+
             btn = ctk.CTkButton(self,
                                 fg_color = 'transparent',
                                 height = c.BUTTON_MAX_HEIGHT,
@@ -72,6 +75,7 @@ class Main(ctk.CTkFrame):
                                 text_color = c.THEME,
                                 hover_color = c.THEME[::-1],
                                 )
+
             btn.grid(row = row,
                      column = col,
                      padx = 2,
@@ -80,7 +84,164 @@ class Main(ctk.CTkFrame):
             btn.grid_propagate(False)
             self.grid_coords[row, col] = btn
 
+    def get_colours(self, parent):
+        """
+        Compare user-submitted word
+        against target and give colour
+        based on placement
 
+        Parameters
+        ----------
+        parent : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        
+        guess = parent.guess_word
+        # Easiest if word is converted to a list
+        target = list(parent.target_word)
+
+        colours = ['' for i in target]
+
+        for i, letter in enumerate(guess):
+            # First check for exact matches
+            if letter == target[i]:
+                colours[i] = c.GREEN
+                # Remove exact matches from word so not double-counted
+                target[i] = None
+
+        for i, letter in enumerate(guess):
+            if colours[i] == '':
+                if letter in target:
+                    colours[i] = c.YELLOW
+                    # Remove partial match from word so not double-counted
+                    target.remove(letter)
+                else:
+                    # Everything else must be grey by default
+                    colours[i] = c.GREY
+        
+        return colours
+
+    def _shrink_box(self, button, max_height):
+        """
+        Simulate first part of box-flipping
+        by decrementing the box height to a minimum
+    
+        Args:
+            button (ctk.CTkButton): The button to be shrunk.
+        """
+              
+        # Should be 100 but may be a bit off
+        # due to window-drawing quirks
+        current_height = button.winfo_height()
+        if current_height > max_height:
+            current_height = max_height
+    
+        while current_height > 1:
+            current_height -= 1
+            button.configure(height=current_height)
+            button.update()
+    
+        # Workaround for button flexing
+        button.configure(height=1, border_color=c.THEME)
+    
+    
+    def _expand_box(self, button, colour, max_height):
+        button.configure(fg_color=colour, hover_color=colour)
+    
+        current_height = button.winfo_height()
+        if current_height != 1:
+            current_height = 1
+    
+        while button.winfo_height() <= max_height:
+            current_height += 1
+            button.configure(height=current_height)
+            button.update()
+    
+        # Workaround for button flexing
+        button.configure(height=max_height, border_color=c.THEME)
+        
+        
+    def flip_box(self, 
+                 button, 
+                 colour, 
+                 max_height=c.BUTTON_MAX_HEIGHT):
+        """
+        Handler for total animation.
+    
+        Args:
+            button (ctk.CTkButton): The button that was clicked.
+            colour (str): The color to be applied to the button.
+        """
+        
+                   
+        # Shrink animation
+        self._shrink_box(button, max_height)
+        # Expand animation
+        self._expand_box(button, colour, max_height)
+
+
+    def lose(self, parent):
+        """
+        Simulate second part of box-flipping
+        by incrementing the box height to the original height
+        Args:
+            button (ctk.CTkButton): The button to be expanded.
+            colour (str): The color to be applied to the button.
+        """
+        
+        box = CTkMessagebox(title = "Try again?",
+                            cancel_button = None,
+                            message = f"The word was {parent.target_word}", 
+                            icon = None, 
+                            option_1 = "Retry", 
+                            option_2 = "Quit",
+                            fade_in_duration=2)
+        box.focus_set()
+        
+        response = box.get()
+        if response == 'Retry':
+            parent.quit_game()
+        elif response == 'Quit':
+            parent.quit_game()
+            
+    def win(self, parent):
+        """
+        Display winning box with option 
+        to retry or quit
+
+        Parameters
+        ----------
+        parent : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        box = CTkMessagebox(parent,
+                            title = "Correct!",
+                            cancel_button = None,
+                            message = f"The word was {parent.target_word}",
+                            icon = None,
+                            option_1 = "Retry", 
+                            option_2 = "Quit",
+                            fade_in_duration=2)
+        box.focus_set()
+        
+        response = box.get()
+        if response == 'Retry':
+            parent.quit_game()
+        elif response == 'Quit':
+            parent.quit_game()
+
+
+# %%
 # Options Frame
 class Options(ctk.CTkFrame):
     """Panel containing in-game options"""
@@ -91,8 +252,6 @@ class Options(ctk.CTkFrame):
                        fg_color = c.THEME[::-1],
                        width = parent.winfo_screenwidth()*0.1,
                        )
-        
-
 
         self.grid(row = 0,
                   column = 0,
@@ -100,8 +259,6 @@ class Options(ctk.CTkFrame):
                   sticky = 'nw',
                   )
         self.grid_propagate(False)
-        # self.grid_columnconfigure((0, 1), weight=2, uniform='tag')
-
         
         self.add_option_label()
         self.add_dark_mode()
@@ -247,7 +404,6 @@ class Keyboard(ctk.CTkFrame):
                   )
         
         self.key_coords = {}
-        
         self.add_keyboard_keys(parent)
         
         # Allows users to type
@@ -258,68 +414,24 @@ class Keyboard(ctk.CTkFrame):
         
     def _decrement_letter_position(self, parent):
         parent.current_position -= 1
+        
+    def _reset_letter_position(self, parent):
+        parent.current_position = 0
 
     def _increment_guess_number(self, parent):
         parent.guess_number += 1
         
     def _decrement_guess_number(self, parent):
         parent.guess_number -= 1
-    
+       
     def _add_letter_to_word(self, parent, letter):
         parent.guess_word += letter
         
     def _remove_letter_from_word(self, parent):
         parent.guess_word = parent.guess_word[:-1]
         
-    def key_pressed(self, event, parent):
-        """
-        Handler for key press event
-
-        Parameters
-        ----------
-        event : TYPE
-            DESCRIPTION.
-        parent : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        None.
-
-        """
-        
-        # Handle on-screen keyboard or physical key presses
-        if isinstance(event, str):
-            if event == '⌫':
-                key = 'BACKSPACE'
-            elif event == '↵':
-                key = 'RETURN'
-            else:
-                key = event
-        else:
-            key = event.keysym.upper()
-        
-        # Avoids attempts to write to non-existent buttons
-        if key in c.ALPHABET and parent.current_position < c.WORD_LENGTH:
-            button = (parent
-                      .main
-                      .grid_coords[parent.guess_number, parent.current_position]
-                      )
-            button.configure(text=key)
-            
-            self._increment_letter_position(parent)
-            self._add_letter_to_word(parent, key)
-            
-        elif key == 'BACKSPACE':
-            if parent.current_position > 0:
-                self._decrement_letter_position(parent)
-                self._remove_letter_from_word(parent)
-                button = (parent
-                          .main
-                          .grid_coords[parent.guess_number, parent.current_position]
-                          )
-                button.configure(text='')
-            
+    def _reset_word(self, parent):
+        parent.guess_word = ''
             
     def add_keyboard_keys(self, parent):
         """
@@ -369,49 +481,126 @@ class Keyboard(ctk.CTkFrame):
         
                  self.key_coords[idx, idy] = letter               
 
-def get_colours(word: str, target: str)-> list:
-    """
-    Compare user-submitted word
-    against target and give colour
-    based on placement
+    def key_pressed(self, event, parent):
+        """
+        Handler for key press event
 
-    Args:
-        word (str): User's guess.
-        target (str): Target word.
+        Parameters
+        ----------
+        event : TYPE
+            DESCRIPTION.
+        parent : TYPE
+            DESCRIPTION.
 
-    Returns:
-        lst_clue (list): Colours of each letter.
+        Returns
+        -------
+        None.
 
-    """
-
-    # Easiest if word is converted to a list
-    lst_target = list(target)
-
-    # Blank list ready to be populated with colours
-    colours = ['' for i in target]
-
-    for i, letter in enumerate(word):
-        # First check for exact matches
-        if letter == target[i]:
-            colours[i] = GREEN
-            # Remove exact matches from word so not double-counted
-            lst_target[i] = None
-
-    for i, letter in enumerate(word):
-
-        if colours[i] == '':
-            if letter in lst_target:
-                colours[i] = YELLOW
-                # Remove partial match so not double-counted
-                lst_target.remove(letter)
+        """
+        
+        # Handle on-screen keyboard or physical key presses
+        if isinstance(event, str):
+            if event == '⌫':
+                key = 'BACKSPACE'
+            elif event == '↵':
+                key = 'RETURN'
             else:
-                # Everything else must be grey by default
-                colours[i] = GREY
-    return colours
+                key = event
+        else:
+            key = event.keysym.upper()
+        
+        if key in c.ALPHABET: 
+            self._add_letter(parent, key)
+                
+        elif key == 'BACKSPACE':
+            self._delete_letter(parent)
 
-# Will display letters if typed or
-# on-screen keyboard used
-# root.bind('<Key>', key_pressed)
+        elif key == 'RETURN':
+            self.submit_guess(parent)
+            
+    def _add_letter(self, parent, key):
+        """
+        Add letter to next available space in grid
+
+        Parameters
+        ----------
+        parent : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        if parent.current_position < c.WORD_LENGTH:
+            button = (parent
+                      .main
+                      .grid_coords[parent.guess_number, parent.current_position]
+                      )
+            button.configure(text=key)
+        
+            self._increment_letter_position(parent)
+            self._add_letter_to_word(parent, key)
+            
+            
+    def _delete_letter(self, parent):
+        """
+        Delete last latter from grid
+
+        Parameters
+        ----------
+        parent : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        grid = parent.main.grid_coords
+        if parent.current_position > 0:
+            self._decrement_letter_position(parent)
+            self._remove_letter_from_word(parent)
+            button = grid[parent.guess_number, parent.current_position]
+            button.configure(text='')
+        
+        
+    def submit_guess(self, parent):
+        """
+        Handler for guess submission
+
+        Parameters
+        ----------
+        key : TYPE
+            DESCRIPTION.
+        parent : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        
+        button_grid = parent.main.grid_coords
+        
+        if not get_definition(parent.guess_word):
+            print(f'{parent.guess_word} not a valid guess')
+            
+        elif parent.current_position == c.WORD_LENGTH:
+            for idx, colour in enumerate(parent.main.get_colours(parent)):
+                button = button_grid[parent.guess_number, idx]
+                parent.main.flip_box(button, colour, c.BUTTON_MAX_HEIGHT)
+            
+            if parent.guess_word == parent.target_word:
+                parent.main.win(parent)
+                           
+            self._increment_guess_number(parent)
+            self._reset_letter_position(parent)
+            self._reset_word(parent)
+            
+        if parent.guess_number == c.NUM_GUESSES:
+            parent.main.lose(parent)
 # %% 
 # App
 class App(ctk.CTk):
@@ -441,8 +630,14 @@ class App(ctk.CTk):
         # Initial values
         self.word_length = c.WORD_LENGTH
         self.number_guesses = c.NUM_GUESSES
+        
+        self.target_word = word_def_pair(self.word_length)[0]
+        
+        print(self.target_word)
+
         self.guess_number = 0
         self.current_position = 0
+
         self.guess_word = ''
         
         
@@ -466,10 +661,8 @@ class App(ctk.CTk):
         self.update()
         self.quit()
         self.destroy()
-        
-        
 
-        
+
 # %%
 if __name__=='__main__':
     app = App()
